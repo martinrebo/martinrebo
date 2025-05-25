b462a3abfdb9ad256d6fefbab1dbcb5b0c4865fb
WarTeamX <martin@warteamx.com>
2025-05-25 08:36:33 +0200

    ➕ install python

---
+++ labs/python/main.py
@@ -0,0 +1,8 @@
+def hello_world():
+    print("Hello, world!")
+
+hello_world()
+# This is a simple Python script that prints "Hello, world!" to the console.
+# It serves as a basic example of how to define and call a function in Python.
+
+# To run this script, you can use the command line:
